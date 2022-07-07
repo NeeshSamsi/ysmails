f2301797687f9128871fa9a268b5c715eddca285
Neesh <avaneeshsamsi@gmail.com>
2022-07-07 13:09:51 +0530

    feat: Add types

---
+++ types/index.ts
@@ -0,0 +1,42 @@
+export interface Recepient {
+  firstName: string
+  lastName?: string
+  email: string
+}
+
+export interface EmailLog extends Recepient {
+  status: EmailStatus
+  error?: any
+}
+
+export interface Attachment {
+  filename: string
+  path: string
+}
+
+export interface MailOptions {
+  to: string
+  from: string
+  subject: string
+  text?: string
+  html?: string
+  attachments?: Attachment[]
+}
+
+export interface EmailFormData {
+  subject: string
+  message: string
+  messageType: MessageType
+  // recepients: Recepient[]
+  recepients: string
+}
+
+export enum MessageType {
+  HTML = "HTML",
+  TEXT = "Text",
+}
+
+export enum EmailStatus {
+  SUCCESS = "Success",
+  ERROR = "Error",
+}
